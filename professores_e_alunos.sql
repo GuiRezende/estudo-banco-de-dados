c35eef0da439b6fb5f50393fcaaa405991462d93
Guilherme Rezende <guiga.rezende@hotmail.com>
2016-09-22 01:12:07 -0300

    Add files via upload

---
+++ professores_e_alunos.sql
@@ -0,0 +1,72 @@
+insert into pessoas values ('99999999999', 'Logan Wolverine', 'M');
+insert into alunos values ('99999999999', 'ra999');
+
+insert into pessoas values ('88888888888', 'Pato Donald', 'M');
+insert into alunos values ('88888888888', 'ra888');
+
+insert into pessoas values ('77777777777', 'Donatelo', 'M');
+insert into alunos values ('77777777777', 'ra777');
+
+insert into pessoas values ('66666666666', 'Ana Belle', 'F');
+insert into alunos values ('66666666666', 'ra666');
+
+insert into pessoas values ('55555555555', 'Bob Marley', 'M');
+insert into alunos values ('55555555555', 'ra555');
+
+insert into pessoas values ('44444444444', 'Joana Dark', 'F');
+insert into alunos values ('44444444444', 'ra444');
+
+insert into pessoas values ('33333333333', 'Mc Fly', 'M');
+insert into alunos values ('33333333333', 'ra333');
+
+insert into pessoas values ('22222222222', 'Dr. Brown', 'M');
+insert into alunos values ('22222222222', 'ra222');
+
+insert into pessoas values ('11111111111', 'Bruce Dickson', 'M');
+insert into alunos values ('11111111111', 'ra111');
+
+insert into pessoas values ('99999999999', 'Milton Leao' , 'M');
+insert into professores values ('99999999999', '0001', 5000);
+
+insert into pessoas values ('88888888888', 'Mariana Torreao' , 'F');
+insert into professores values ('88888888888', '0002', 3700);
+
+insert into pessoas values ('77777777777', 'Aparecido Cidao' , 'M');
+insert into professores values ('77777777777', '0003', 4250);
+
+insert into pessoas values ('66666666666', 'Charles Bob' , 'M');
+insert into professores values ('66666666666', '0004', 4700);
+
+insert into pessoas values ('55555555555', 'Marcelo' , 'M');
+insert into professores values ('55555555555', '0005', 3700);
+
+insert into pessoas values ('44444444444', 'Julio Edgardo' , 'M');
+insert into professores values ('44444444444', '0006', 4000);
+
+insert into pessoas values ('33333333333', 'Lucas Crosue' , 'M');
+insert into professores values ('33333333333', '0007', 3550);
+
+insert into pessoas values ('22222222222', 'Ivana Ferraz' , 'F');
+insert into professores values ('22222222222', '0008', 4000);
+
+insert into pessoas values ('11111111111', 'Samenis Costa' , 'F');
+insert into professores values ('11111111111', '0009', 5000);
+
+insert into pessoas values ('12312312312', 'Marcos Prado' , 'M');
+insert into professores values ('12312312312', '0010', 6000);
+
+insert into pessoas values ('12341234123', 'Marcus Vinicius' , 'M');
+insert into professores values ('12341234123', '0011', 6000);
+
+insert into pessoas values ('12345123451', 'Sinval Sousa' , 'M');
+insert into professores values ('12345123451', '0012', 4500);
+
+insert into pessoas values ('33322244412', 'Jarbas Farias' , 'M');
+insert into professores values ('33322244412', '0013', 5000);
+
+insert into pessoas values ('98765432121', 'Iggor Lincoln' , 'M');
+insert into professores values ('98765432121', '0014', 3000);
+
+insert into pessoas values ('55566655566', 'Stenio Soares' , 'M');
+insert into professores values ('55566655566', '0015', 5000);
+
